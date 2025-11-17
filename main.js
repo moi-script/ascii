5996714331a4aea3f550f045898f2944d92ce0b0
lowkeyCodee <moisesnugal24@gmail.com>
2025-11-17 13:57:56 +0800

    Initial test

---
+++ main.js
@@ -0,0 +1,12 @@
+import  im2  from  "image-and-video-to-ascii";
+
+// console.log(
+// await im2.getImageAscii("test.png", {
+//     height :20,
+//     width : 20,
+// }))
+
+// im2.showVideo('nora.mp4');
+
+
+
